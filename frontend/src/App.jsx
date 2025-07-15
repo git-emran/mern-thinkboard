69c8064b24e34e655324b6e2b6a8fbe6dd642eb9
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-15 19:25:54 +0600

    tailwindcss and react hot toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
@@ -6,11 +5,13 @@ import NoteDetailsPage from "./pages/NoteDetailsPage";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/note/:id" element={<NoteDetailsPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
   );
 };
 
