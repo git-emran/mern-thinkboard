3bf45d7d983338b5e7b68c4c0dfc1235fea256e8
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-16 22:04:54 +0600

    Add CORS support and fetch notes functionality; update Navbar with link to create new note

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import mongoDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT || 5001;
 
 //middlware
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
