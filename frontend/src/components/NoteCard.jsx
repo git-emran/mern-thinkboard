49e12ff84b50ed4a656d4c2796496ec6a5231373
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-17 09:56:04 +0600

    note card trash icon added

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,4 +1,4 @@
-import { PenSquareIcon } from "lucide-react";
+import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
 
@@ -17,7 +17,9 @@ const NoteCard = ({ note }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
-            <button className="btn btn-ghost"></button>
+            <button className="btn btn-ghost btn-xs text-error">
+              <Trash2Icon className="size-4" />
+            </button>
           </div>
         </div>
       </div>
