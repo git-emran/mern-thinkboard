edc38a7bd2c11ad35b5c17f9c983db264abf1357
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-18 00:48:17 +0600

    minor updates

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -36,8 +36,8 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      {isRateLimited && <RateLimitedUI />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
+        {isRateLimited && <RateLimitedUI />}
         {loading && (
           <div className="text-center text-primary py-10">
             {" "}
