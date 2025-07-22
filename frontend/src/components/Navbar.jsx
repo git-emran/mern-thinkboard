a7d7f2b041bec910764666b7805e72e30f594710
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-22 23:36:17 +0600

    size fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           </h1>
           <div className="flex items-center gap-4">
             <Link to={"/create"} className="btn btn-primary">
-              <PlusIcon className="size-5" />
+              <PlusIcon className="size-4" />
               <span>New Note</span>
             </Link>
           </div>
